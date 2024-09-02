7f4a3fba2a35f0df9ca54d824d0ee60a7fbf322f
jungyoon <sophia7625@naver.com>
2024-09-02 12:29:40 +0900

    제어문

--- lesson05_control/src/lesson05_control/SwitchEx.java
+++ lesson05_control/src/lesson05_control/SwitchEx.java
@@ -5,18 +5,32 @@ public class SwitchEx {
 		int month = (int)(Math.random() * 12) + 1;
 		
 		System.out.println(month + "월은");
-		switch(month) {
-			case 3: case 4: case 5:
-				System.out.println("봄");
-				break;
-			case 6: case 7: case 8:
-				System.out.println("여름");
-				break;
-			case 9: case 10: case 11:
-				System.out.println("가을");
-				break;
-			default:
-				System.out.println("겨울");
-		}
+//		switch(month) {
+//			case 3: case 4: case 5:
+//				System.out.println("봄");
+//				break;
+//			case 6: case 7: case 8:
+//				System.out.println("여름");
+//				break;
+//			case 9: case 10: case 11:
+//				System.out.println("가을");
+//				break;
+//			default:
+//				System.out.println("겨울");
+//		}
+		
+		switch(month / 3) {
+		case 1:
+			System.out.println("봄");
+			break;
+		case 2:
+			System.out.println("여름");
+			break;
+		case 3:
+			System.out.println("가을");
+			break;
+		default:
+			System.out.println("겨울");
+	}
 	}
 }
