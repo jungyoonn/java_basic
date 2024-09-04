fa94cdb791b15579bfc84c9d5aded6323f0752cf
jungyoonn <sophia7625@naver.com>
2024-09-04 16:42:25 +0900

    객체지향

---
+++ lesson07_oop/src/lesson07_oop/Car.java
@@ -0,0 +1,21 @@
+package lesson07_oop;
+
+public class Car {
+	String color;
+	String company;
+	String type;
+	
+	/**
+	 * 전진하는 메서드
+	 */
+	public void go() {
+		System.out.println(" 전진하다");
+	}
+	
+	/**
+	 * 후진하는 메서드
+	 */
+	public void back() {
+		System.out.println(" 후진하다");
+	}
+}
--- lesson07_oop/src/lesson07_oop/MemberMain.java
+++ lesson07_oop/src/lesson07_oop/MemberMain.java
@@ -1,14 +1,29 @@
 package lesson07_oop;
 
 public class MemberMain {
+	Member member = new Member();
+	int a = 10;
 	public static void main(String[] args) {
 		Member m = new Member();
+		m.age = 10;
+		m.kor = 90;
+		m.eng = 60;
+		m.mat = 80;
+		//System.out.println(m.kor + m.eng + m.mat);
+		System.out.println(m.total());
 		Member m2 = new Member();
+		m2.kor = 80;
+		m2.eng = 100;
+		m2.mat = 90;
+		//System.out.println(m2.kor + m2.eng + m2.mat);
+		System.out.println(m2.total());
 		
 		if(m == m2) {
 			System.out.println("m, m2는 같은 객체");
 		} else {
 			System.out.println("m, m2는 다른 객체");
 		}
+		
+		
 	}
 }
