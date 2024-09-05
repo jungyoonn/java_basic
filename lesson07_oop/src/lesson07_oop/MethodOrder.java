23320d7570f796faf1140198bbdd9dd1fd030e81
jungyoonn <sophia7625@naver.com>
2024-09-05 16:34:20 +0900

    오버로딩

---
+++ lesson07_oop/src/lesson07_oop/MethodOrder.java
@@ -0,0 +1,27 @@
+package lesson07_oop;
+
+public class MethodOrder {
+	public static void main(String[] args) {
+		System.out.println("main start");
+		MethodEx me = new MethodEx();
+		me.one();
+		System.out.println("main end");
+	}
+}
+
+class MethodEx {
+	void one() {
+		System.out.println("one start");
+		two();
+		System.out.println("one end");
+	}
+	void two() {
+		System.out.println("two start");
+		three();
+		System.out.println("two end");
+	}
+	void three() {
+		System.out.println("three");
+	}
+	
+}
