64d7f3fd8bff42f6e729a66b174f74cdfc30dc02
jungyoonn <sophia7625@naver.com>
2024-09-05 11:38:11 +0900

    배열과제

--- lesson06_array/src/day2/Exer.java
+++ lesson06_array/src/day2/Exer.java
@@ -26,22 +26,54 @@ public class Exer {
 		// 입력 글자 중 q를 입력하면 루프 종료
 		// 사람 이름을 문자열 배열로 만들어서 (배열의 확장)
 		// 프로그램 종료 직전 확인
-		String[] names = new String[100];
+		String[] names = new String[2];
+		String[] printNames = {}; 
 		int cnt = 0;
 		
+//		while(true) {
+//			System.out.print("이름 입력 > ");
+//			String inputName = scan.nextLine();
+//			if(inputName .equals("q")) {
+//				System.out.println("입력을 종료합니다");
+//				break;
+//			}
+//			names[cnt] = inputName;
+//			cnt++;
+//			
+//			if(cnt > names.length) {
+//				names = new String[cnt * 2];
+//				String[] tmp = new String[cnt];
+//				printNames = names;
+//				//System.arraycopy(names, 0, printNames, 0, cnt);
+//				
+//			}
+//		}
+
+//		String[] printName = new String[cnt];
+//		System.arraycopy(names, 0, printName, 0, cnt);
+//		System.out.println(Arrays.toString(printName));
+//		
+
 		while(true) {
 			System.out.print("이름 입력 > ");
 			String inputName = scan.nextLine();
-			if(inputName .equals("q")) {
+			if(inputName.equals("q")) {
 				System.out.println("입력을 종료합니다");
 				break;
+			}
+			// 배열 늘리기
+			if(names.length == cnt) {
+				names = Arrays.copyOf(names, cnt*2);
 			}
 			names[cnt] = inputName;
 			cnt++;
 		}
-
-		String[] printName = new String[cnt];
-		System.arraycopy(names, 0, printName, 0, cnt);
-		System.out.println(Arrays.toString(printName));
+		
+		for(String name : names) {
+			if(name == null) {
+				break;
+			}
+			System.out.print(name + "  ");
+		}
 	}
 }
