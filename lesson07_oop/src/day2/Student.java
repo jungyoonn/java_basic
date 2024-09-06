f78c2b3f4cb4d1d3a71aaf55d45f7cb0975541ea
jungyoonn <sophia7625@naver.com>
2024-09-06 15:04:44 +0900

    생성자 오버로딩

--- lesson07_oop/src/day2/Student.java
+++ lesson07_oop/src/day2/Student.java
@@ -8,17 +8,26 @@ public class Student {
 	
 	// 커스텀 생성자를 만들 경우 기본 생성자가 자동으로 만들어지지 않음
 	Student(int i, String s) { // 생성자 오버로딩
+		// 생성자의 첫줄은 super(), this()
 		no = i;
 		name = s;
 	}
 	
 	Student(String s, int i) { // 위와 다른 생성자로 인식
+		this(s);
 		no = i;
-		name = s;
+		//name = s;
 	}
 
 	Student(String s) { // 생성자 오버로딩
 		name = s;
 	}
 	
+	void printThis() {
+		System.out.println(this);
+	}
+	
+	Student returnThis() {
+		return this;
+	}
 }
--- lesson07_oop/src/day2/StudentMain.java
+++ lesson07_oop/src/day2/StudentMain.java
@@ -12,5 +12,12 @@ public class StudentMain {
 		Student st2 = new Student(10, "길동이");
 		System.out.println(st2.no);
 		System.out.println(st2.name);
+		
+		Student st3 = new Student("소똥이", 30);
+		System.out.println(st3.no);
+		System.out.println(st3.name);
+		
+		st3.printThis();
+		st3.returnThis();
 	}
 }
