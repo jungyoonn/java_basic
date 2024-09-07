42a511b638ba16a85a6c57fd625b866566d2ac1c
jungyoonn <sophia7625@naver.com>
2024-09-07 17:33:06 +0900

    삭제 유효성 검사

--- student/src/student/StudentService.java
+++ student/src/student/StudentService.java
@@ -3,7 +3,7 @@ package student;
 import java.util.Arrays;
 // Logic
 public class StudentService {
-	static Student[] students = new Student[5];
+	static Student[] students = new Student[2];
 	static int cnt;
 	
 	{
@@ -20,7 +20,7 @@ public class StudentService {
 //		students[cnt].name = StudentUtils.nextLine("추가할 이름"); 
 //		students[cnt].kor = StudentUtils.nextInt("국어 점수"); 
 //		students[cnt].eng = StudentUtils.nextInt("영어 점수"); 
-//		students[cnt++].mat = StudentUtils.nextInt("수학 점수"); 
+//		students[cnt++].mat = StudentUtils.nextInt("수학 점수");
 	}
 	
 	void list() {
@@ -60,6 +60,7 @@ public class StudentService {
 				break;
 			}
 		}
+		student.update();
 	}
 	
 	void remove() {
@@ -67,7 +68,19 @@ public class StudentService {
 		
 		for(int i = 0; i < cnt; i++) {
 			if(students[i].no == rNo) {
-				students[i].no = -1;				
+				String yn = StudentUtils.nextLine(students[i].name +" 학생을 삭제하시겠습니까? (y/n)");
+				if(yn.equals("y")) {
+				students[i].no = -1;	
+				System.out.println("삭제를 완료했습니다.");
+				System.out.println();
+				} else if(yn.equals("n")) {
+					System.out.println("초기 화면으로 돌아갑니다.");
+					System.out.println();
+					break;
+				} else {
+					System.out.println("입력 오류! 초기 화면으로 돌아갑니다.");
+					System.out.println();
+				}
 			}
 		}
 	}
