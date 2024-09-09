c375ff16f9ea2c1854409fb8dd0364dc307e09d2
jungyoonn <sophia7625@naver.com>
2024-09-09 11:01:01 +0900

    강사님 풀이 적용

--- student/src/student/StudentService.java
+++ student/src/student/StudentService.java
@@ -1,5 +1,7 @@
 package student;
 
+import static student.StudentUtils.*;
+
 import java.util.Arrays;
 // Logic
 public class StudentService {
@@ -15,12 +17,20 @@ public class StudentService {
 		if(cnt == students.length) {
 			students = Arrays.copyOf(students, cnt*2);
 		} 
-		students[cnt++] = StudentUtils.addInput();
+//		students[cnt++] = addInput();
+		
 //		students[cnt].no = StudentUtils.nextInt("추가할 학번"); 
 //		students[cnt].name = StudentUtils.nextLine("추가할 이름"); 
 //		students[cnt].kor = StudentUtils.nextInt("국어 점수"); 
 //		students[cnt].eng = StudentUtils.nextInt("영어 점수"); 
 //		students[cnt++].mat = StudentUtils.nextInt("수학 점수");
+		
+		int no = nextInt("추가할 학번");
+		String name = nextLine("추가할 이름");
+		int kor = nextInt("국어 점수");
+		int eng = nextInt("영어 점수");
+		int mat = nextInt("수학 점수");
+		students[cnt++] = new Student(no, name, kor, eng, mat);
 	}
 	
 	void list() {
@@ -45,8 +55,67 @@ public class StudentService {
 	
 	// 이름, 점수 
 	void modify() {
+		Student student = findByNo();
+		if(student == null) {
+			System.out.println("학생을 찾을 수 없습니다.");
+			return;
+		}
+		
+		student.name = nextLine("수정할 이름");
+		student.kor = nextInt("수정할 국어 점수");
+		student.eng = nextInt("수정할 영어 점수");
+		student.mat = nextInt("수정할 수학 점수");
+		
+//			if(students[i].no == mNo) {
+//				students[i].name = StudentUtils.nextLine("수정할 이름");
+//				students[i].kor = StudentUtils.nextInt("수정할 국어 점수");
+//				students[i].eng = StudentUtils.nextInt("수정할 영어 점수");
+//				students[i].mat = StudentUtils.nextInt("수정할 수학 점수");
+//			
+	}
+	
+	void remove() {
+		Student student = findByNo();
+		if(student == null) {
+			System.out.println("학생을 찾을 수 없습니다.");
+			return;
+		}
+		
+		for(int i = 0; i < cnt; i++) {
+			if(students[i] == student) {
+				System.arraycopy(students, i+1, students, i, cnt-- -i - 1);
+			}
+		}
+		
+//		String yn = nextLine(student.name + " 학생의 정보를 삭제하시겠습니까?");
+//		if(yn.equals("y")) {
+//			student.no = -1;
+//			System.out.println("삭제를 완료했습니다.");
+//		} else {
+//			System.out.println("삭제를 취소합니다. 초기 화면으로 돌아갑니다.");
+//		}
+	}
+	
+	// 중복된 기능 분리
+	Student findByNo() {
 		Student student = null;
-		int mNo = StudentUtils.nextInt("수정할 학생의 학번 입력");
+		int no = nextInt("학생의 학번 입력 > ");
+		
+		for(int i = 0; i < cnt; i++) {
+			if(no == students[i].no) {
+				student = students[i];
+			}
+		}
+		
+		return student;
+	}
+	
+	
+	
+	
+	void modify2() {
+		Student student = null;
+		int mNo = nextInt("수정할 학생의 학번 입력");
 		int i;
 		for(i = 0; i < cnt; i++) {
 //			if(students[i].no == mNo) {
@@ -62,13 +131,12 @@ public class StudentService {
 		}
 		student.update();
 	}
-	
-	void remove() {
-		int rNo = StudentUtils.nextInt("삭제할 학생의 학번 입력");
+	void remove2() {
+		int rNo = nextInt("삭제할 학생의 학번 입력");
 		
 		for(int i = 0; i < cnt; i++) {
 			if(students[i].no == rNo) {
-				String yn = StudentUtils.nextLine(students[i].name +" 학생을 삭제하시겠습니까? (y/n)");
+				String yn = nextLine(students[i].name +" 학생을 삭제하시겠습니까? (y/n)");
 				if(yn.equals("y")) {
 				students[i].no = -1;	
 				System.out.println("삭제를 완료했습니다.");
--- student/src/student/StudentUtils.java
+++ student/src/student/StudentUtils.java
@@ -20,12 +20,12 @@ public class StudentUtils {
 	}
 	
 	// 새로운 학생 등록
-	static Student addInput() {
-		int no = nextInt("추가할 학번");
-		String name = nextLine("추가할 이름");
-		int kor = nextInt("국어 점수");
-		int eng = nextInt("영어 점수");
-		int mat = nextInt("수학 점수");
-		return new Student(no, name, kor, eng, mat);
-	}
+//	static Student addInput() {
+//		int no = nextInt("추가할 학번");
+//		String name = nextLine("추가할 이름");
+//		int kor = nextInt("국어 점수");
+//		int eng = nextInt("영어 점수");
+//		int mat = nextInt("수학 점수");
+//		return new Student(no, name, kor, eng, mat);
+//	}
 }
