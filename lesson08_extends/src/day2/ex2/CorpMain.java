d4aebb40678bd30cb84fce3c54ea7f8527cb2c5d
jungyoonn <sophia7625@naver.com>
2024-09-09 14:41:24 +0900

    추상 클래스

--- lesson08_extends/src/day2/ex2/CorpMain.java
+++ lesson08_extends/src/day2/ex2/CorpMain.java
@@ -10,10 +10,21 @@ public class CorpMain {
 		employees[idx++] = new Designer("소똥이");
 		employees[idx++] = new Designer("말똥이");
 		
-		for(Employee e : employees) {
+		for(Employee e : employees) { // 오버라이드 한 경우
 			System.out.println("==========" + e.name + "의 하루===========");
 			e.startWork();
 			e.work();
+			e.endWork();
+		}
+		
+		for(Employee e : employees) { // 오버라이드 안 한 경우 (부모 클래스에 work() 메서드가 없는 경우)
+			System.out.println("==========" + e.name + "의 하루===========");
+			e.startWork();
+			if(e instanceof Programmer) {
+				((Programmer)e).work();
+			} else if (e instanceof Designer) {
+				((Designer)e).work();
+			}
 			e.endWork();
 		}
 	}
--- lesson08_extends/src/day2/ex2/Employee.java
+++ lesson08_extends/src/day2/ex2/Employee.java
@@ -1,6 +1,6 @@
 package day2.ex2;
 
-public class Employee {
+abstract class Employee {
 	String name;
 	
 	public Employee(String name) {
@@ -11,9 +11,13 @@ public class Employee {
 		System.out.println("출근합니다");
 	}
 	
-	void work() {
-		System.out.println("업무합니다");
-	}
+//	void work() {
+//		System.out.println("업무합니다");
+//	} 추상 메서드로 대체 가능
+	
+	abstract void work(); // 클래스 앞에도 abstract를 붙여야 함
+						  // 오버라이드를 강제로 하는 목적의 추상 메서드 (상속 목적)
+						  // 추상 메서드가 하나라도 있으면 추상 클래스가 된다
 	
 	void endWork() {
 		System.out.println("퇴근합니다");
