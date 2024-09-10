5b389ac0f8c1de10fc280f1cdbbfd772ccf0c664
jungyoonn <sophia7625@naver.com>
2024-09-10 13:02:22 +0900

    인터페이스

--- lesson09_interface/src/lesson09_interface/Fax.java
+++ lesson09_interface/src/lesson09_interface/Fax.java
@@ -1,6 +1,7 @@
 package lesson09_interface;
 
 public interface Fax { // 인터페이스는 다중 상속이 가능!
+					   // 기본적으로 abstract이다
 	String FAX_NUMBER = "01-1234-5678";
 	void send(String tel);
 	void receive(String tel);
