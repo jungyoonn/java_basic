40b4fbc6638c06db598358b2359d207123829b4d
jungyoonn <sophia7625@naver.com>
2024-09-12 16:56:05 +0900

    Math

---
+++ lesson12_api/src/lesson12_api/MathEx.java
@@ -0,0 +1,15 @@
+package lesson12_api;
+
+public class MathEx {
+	public static void main(String[] args) {
+		System.out.println(Math.E);
+		System.out.println(Math.PI);
+		
+		System.out.println(Math.abs(5));
+		System.out.println(Math.abs(-5)); // 절댓값
+		
+		System.out.println(Math.floor(3.141592)); // 내림 (정수)
+		System.out.println(Math.ceil(3.141592)); // 올림 (정수)
+		System.out.println(Math.round(3.141592)); // 반올림 (정수)
+	}
+}
