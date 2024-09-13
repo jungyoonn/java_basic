1b5c8e0d9abfdbb39691575a46e7b224d86b5380
jungyoonn <sophia7625@naver.com>
2024-09-13 18:08:25 +0900

    명절 과제

---
+++ lesson12_api/src/lesson12_api/Exer.java
@@ -0,0 +1,18 @@
+package lesson12_api;
+
+public class Exer {
+	public static void main(String[] args) {
+		// 달력 출력
+		// 이전달, 다음달, 이전해, 다음해, 특정 연월 입력 시 해당 연월 달력 출력
+		
+		// url 분석
+		// String class만 사용
+		String str = "";
+		// 자르기
+		// 1. 프로토콜 (https://)
+		// 2. 도메인 search.naver.com
+		// 3. file name (? 앞까지)
+		// 4. 쿼리 스트링 (&로 잘라서)
+		// 4-1. 파라미터와 밸류 쌍으로 묶어서 분리
+	}
+}
