f7900d0cbd26e8a196f88a229762bc3022d9ff00
jungyoonn <sophia7625@naver.com>
2024-09-13 21:53:55 +0900

    과제 중간 저장

--- lesson12_api/src/lesson12_api/ExerUtils.java
+++ lesson12_api/src/lesson12_api/ExerUtils.java
@@ -8,7 +8,7 @@ static Scanner scan = new Scanner(System.in);
 	// 문자열 받아오기
 	static String nextLine(String msg) {
 		System.out.print(msg + " > ");
-		System.out.println("\n===============================================");
+		System.out.println("\n============================================================");
 
 		return scan.nextLine();
 	}
@@ -16,5 +16,25 @@ static Scanner scan = new Scanner(System.in);
 	// 숫자 받아오기
 	static int nextInt(String msg) {
 		return Integer.parseInt(nextLine(msg));
+	}
+	
+	public static void calPrint(int year, int month, int startDay, int lastDay) {
+		System.out.println("시작 요일 ::: " + startDay + "  끝 날짜 ::: " + lastDay);
+		System.out.println("\t" + year + "년 " + month + "월");
+		System.out.println("  일  월  화  수  목  금  토");
+		int cnt = startDay;
+
+		for(int i = 2 - startDay; i <= lastDay; i++, cnt++) {
+			if (i <= 0) {
+				System.out.printf("%-4c", ' ');
+			} else {
+				System.out.printf("%4d", i);
+				if (i % 7 == 7 - startDay) {
+					System.out.println();
+				}
+			}
+		}
+		System.out.println(); 
+		System.out.println();
 	}
 }
