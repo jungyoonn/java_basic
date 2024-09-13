101f3d787fbc987a80532a16d0ac6cfc1aa16f76
jungyoonn <sophia7625@naver.com>
2024-09-13 17:36:59 +0900

    불필요한 코드 제거

--- marble/src/marble/MarbleMain.java
+++ marble/src/marble/MarbleMain.java
@@ -2,10 +2,6 @@ package marble;
 
 public class MarbleMain {
 	public static void main(String[] args) {
-		String names = "시작 > 방콕 > 동전던지기 > 베이징 > 포츈카드 > 독도 > 두바이 > 카이로 > 무인도 > 발리 > 도쿄 > 시드니 > 포츈카드 > 퀘백 > 하와이 > 상파울루 > 올림픽 > 프라하 > 푸켓 > 베를린 > 포츈카드 > 모스크바 > 제네바 > 로마 > 세계여행 > 타히티 > 런던 > 파리 > 포츈카드 > 뉴욕 > 국세청 > 서울";
-		String[] ns = names.split(" > ");
-//		System.out.println(Arrays.toString(ns)); 확인용
-		
 		System.out.println("어서오세요 모두의 마블입니다");
 		System.out.println();
 		
