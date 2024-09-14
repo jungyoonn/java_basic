e3636d59f5ad9bc21584f2bb69537ba62a6bb7e7
sophi <sophia7625@naver.com>
2024-09-15 02:05:35 +0900

    스트링

--- lesson12_api/src/lesson12_api/ExerString.java
+++ lesson12_api/src/lesson12_api/ExerString.java
@@ -1,13 +1,14 @@
 package lesson12_api;
 
-public class Exer {
+public class ExerString {
 	public static void main(String[] args) {
 		// 달력 출력
 		// 이전달, 다음달, 이전해, 다음해, 특정 연월 입력 시 해당 연월 달력 출력
 		
 		// url 분석
 		// String class만 사용
-		String str = "";
+		String str = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%B2%84%EB%B8%94%EC%A0%95%EB%A0%AC";
+		String tmpStr = "";
 		// 자르기
 		// 1. 프로토콜 (https://)
 		// 2. 도메인 search.naver.com
