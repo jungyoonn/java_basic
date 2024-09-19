fadc3e737dfb34ed819a691ab82ef7ad25205159
jungyoonn <sophia7625@naver.com>
2024-09-19 14:48:29 +0900

    Set, HashCode

---
+++ lesson13_collection/src/lesson13_collection/SetEx2.java
@@ -0,0 +1,50 @@
+package lesson13_collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetEx2 {
+	public static void main(String[] args) {
+		Set<Object> set = new HashSet<>();
+		set.add("가");
+		set.add("가");
+		System.out.println(set);
+		Member member = new Member("홍길동", 40);
+		set.add(member);
+		set.add(new Member("홍길동", 40));
+		System.out.println(set);
+		
+		System.out.println("홍길동".hashCode());
+		System.out.println(new String("홍길동").hashCode());
+		// 둘이 해시 코드가 같다
+	}
+}
+
+class Member {
+	String name;
+	int age;
+	public Member(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		return name.hashCode() + age;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null || !(obj instanceof Member)) {
+			return false;
+		}
+		Member member = (Member)obj;
+		return name.equals(member.name) && age == member.age;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
+}
