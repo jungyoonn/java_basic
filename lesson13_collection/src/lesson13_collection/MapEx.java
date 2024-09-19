6860ff74c5399c3798e89ad9e2bbbcf47c3ec3f7
jungyoonn <sophia7625@naver.com>
2024-09-19 16:56:05 +0900

    Map

--- lesson13_collection/src/lesson13_collection/MapEx.java
+++ lesson13_collection/src/lesson13_collection/MapEx.java
@@ -1,7 +1,12 @@
 package lesson13_collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class MapEx {
 	public static void main(String[] args) {
@@ -14,5 +19,27 @@ public class MapEx {
 		System.out.println(map.get("D")); // 없으면 null 리턴
 		System.out.println(map.get("A")); // key의 value를 리턴 = 1 (연산도 가능)
 		System.out.println(map.size());
+		
+		// 탐색 (Map은 for로 탐색 불가함)
+		// 1.
+		Set<Map.Entry<String, Integer>> entries = map.entrySet();
+		for(Map.Entry<String, Integer> entry : entries) {
+			System.out.println(entry.getKey() + " :: " + entry.getValue());
+		}
+		// 2.
+		Set<String> keys = map.keySet();
+		System.out.println(keys);
+		for(String key : keys) {
+			System.out.println(key + " :: " + map.get(key));
+		}
+		
+		Collection<Integer> collection = map.values(); // 밸류 값만 뽑기
+		System.out.println(collection);
+		
+		List<Integer> list = new ArrayList<Integer>(collection);
+		Set<Integer> set = new HashSet<Integer>(collection);
+		
+		System.out.println(list);
+		System.out.println(set);
 	}
 }
