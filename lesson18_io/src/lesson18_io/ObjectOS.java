c4729a4ca7f13567cb5d95e3ca4787d628b827b4
jungyoonn <sophia7625@naver.com>
2024-09-23 17:36:58 +0900

    object에 member 정보 넣기

--- lesson18_io/src/lesson18_io/ObjectOS.java
+++ lesson18_io/src/lesson18_io/ObjectOS.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,5 +23,27 @@ public class ObjectOS {
 		System.out.println(ois.readObject());
 		System.out.println(ois.readObject());
 //		System.out.println(ois.readObject()); write 한 만큼만 read 할 수 있으므로 출력이 안 됨
+		
+		List<Member> members = new ArrayList<Member>(Arrays.asList(new Member("홍길동", 40), new Member("김길동", 20)));
+
+		oos.writeObject(members); // 에러가 나는 이유? 직렬화 이슈 (class에 Serializable implements 필수)
+		List<Member> result = (List<Member>)ois.readObject();
+		result.forEach(System.out::println);
+	}
+}
+
+class Member implements Serializable {
+	String name;
+	int age;
+	
+	public Member(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
 	}
 }
