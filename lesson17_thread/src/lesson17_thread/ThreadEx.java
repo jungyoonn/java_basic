9259f87cd9736f31099a4f31905f6382eb941c99
jungyoonn <sophia7625@naver.com>
2024-09-23 12:17:12 +0900

    프로세스와 스레드

---
+++ lesson17_thread/src/lesson17_thread/ThreadEx.java
@@ -0,0 +1,31 @@
+package lesson17_thread;
+
+public class ThreadEx extends Thread {
+	public static void main(String[] args) {
+		new ThreadExtend().start();
+		new Thread(new RunnableImpl()).start();
+		// start() : 멀티 스레드를 시작하는 메서드
+		
+		System.out.println("main 종료 " + currentThread().getName());
+	}
+}
+
+class ThreadExtend extends Thread {
+	@Override
+	public void run() {
+		System.out.println("Thread 상속 ");
+		for(int i = 0; i < 50; i++) {
+			System.out.println("Thread 상속 " + i + currentThread().getName());
+		}
+	}
+}
+
+class RunnableImpl implements Runnable {
+	@Override
+	public void run() {
+		System.out.println("Runnable 구현 ");
+		for(int i = 0; i < 50; i++) {
+			System.out.println("Runnable 구현 " + i + Thread.currentThread().getName());
+		}
+	}
+}
